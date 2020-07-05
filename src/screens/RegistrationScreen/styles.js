b8b7612939d96b396439b36f6f0f8ab9dd5b7ad2
James Ugbanu <jamesugbanu@gmail.com>
2020-07-05 18:43:14 +0100

    add login and signup function

--- src/screens/RegistrationScreen/styles.js
+++ src/screens/RegistrationScreen/styles.js
@@ -10,8 +10,8 @@ export default StyleSheet.create({
     },
     logo: {
         flex: 1,
-        height: 120,
-        width: 90,
+        height: 84,
+        width: 220,
         alignSelf: "center",
         margin: 30
     },
